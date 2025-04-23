fe013c06eedd4c86c0afa833480057701bc8f442
Uwe Duldinger <uwe.duldinger@gmx.de>
2025-04-23 13:06:40 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HookfyGenerator from "./components/HookfyGenerator";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <HookfyGenerator />
+    </div>
+  );
+}
+
+export default App;
